7d7a3f270d60b670102836808688e7f171f0c71d
Kris Burke <kris.burke611@gmail.com>
2016-02-06 19:41:58 -0500

    Attempt at getting all messages.

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,11 +1,28 @@
 var models = require('../models');
-var bluebird = require('bluebird');
+var Promise = require('bluebird');
+
+/*
+
+data = {result: [ {text: '', username:'',  } ]}
+
+
+*/
+
 
 
 
 module.exports = {
   messages: {
-    get: function (req, res) {}, // a function which handles a get request for all messages
+    get: function (req, res) {
+      //writehead
+      
+      var messages = models.messages.get(function(result){
+        //console.log(result);
+        res.end(JSON.stringify(result));
+      });
+      //console.log(messages._results);
+      res.end();
+    }, // a function which handles a get request for all messages
     post: function (req, res) {} // a function which handles posting a message to the database
   },
 
